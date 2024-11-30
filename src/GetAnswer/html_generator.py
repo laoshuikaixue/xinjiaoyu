79209c1c244774830e9309874f5114413dadc82b
LaoShui <79132480+laoshuikaixue@users.noreply.github.com>
2024-11-30 18:10:47 +0800

    feat: Improve html_generator

--- src/GetAnswer/html_generator.py
+++ src/GetAnswer/html_generator.py
@@ -213,7 +213,12 @@ def json_to_html(json_data, template_name):
         html_output += "</div><hr>"
 
     # 添加页脚
-    html_output += "<div class='footer'>https://github.com/laoshuikaixue/xinjiaoyu<br>温馨提示：仅供学习使用，请勿直接抄袭答案。</div>"
+    html_output += """
+    <div class="footer">
+    <p>GitHub: <a href="https://github.com/laoshuikaixue/xinjiaoyu" target="_blank" style="color: #3498DB;">https://github.com/laoshuikaixue/xinjiaoyu</a><br>
+    温馨提示：仅供学习使用，请勿直接抄袭答案。</p>
+    </div>
+    """
 
     # 关闭HTML标签
     html_output += "</body></html>"
